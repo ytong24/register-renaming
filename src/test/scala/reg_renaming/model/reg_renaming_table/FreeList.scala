347e9a424b4ac86b59467cccad71496f57618802
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-03-04 21:35:55 -0800

    implement FreeList

--- src/test/scala/reg_renaming/model/reg_renaming_table/FreeList.scala
+++ src/test/scala/reg_renaming/model/reg_renaming_table/FreeList.scala
@@ -1,5 +1,29 @@
+/** *************************************************************************************
+ * File         : RegFile.scala
+ * Authors      : Yinyuan Zhao, Yan Tong
+ * Date         : 03/04/2024
+ * Description  : Scala implementation of Free List
+ * ************************************************************************************* */
+
 package reg_renaming.model.reg_renaming_table
 
-object FreeList {
+import scala.collection.mutable
 
+class FreeList(config: RegRenamingTableConfig) {
+  private val _freeListStack = mutable.Stack[Int](0 until config.ptagNum: _*)
+
+  def push(ptag: Int): Unit = {
+    require(ptag >= 0 && ptag < config.ptagNum, "ptag out of range")
+    _freeListStack.push(ptag)
+  }
+
+  def pop(): Int = {
+    require(_freeListStack.nonEmpty, "Free list is empty")
+    _freeListStack.pop()
+  }
+
+  def size: Int = {
+    _freeListStack.size
+  }
 }
+
