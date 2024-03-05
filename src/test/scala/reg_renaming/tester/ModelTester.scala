2d5b6467b62275aaafe36baea17829542e93cb6f
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-03-04 22:51:25 -0800

    add RegFile test cases

--- src/test/scala/reg_renaming/tester/ModelTester.scala
+++ src/test/scala/reg_renaming/tester/ModelTester.scala
@@ -2,7 +2,7 @@ package reg_renaming.tester
 
 import chiseltest.ChiselScalatestTester
 import org.scalatest.flatspec.AnyFlatSpec
-import reg_renaming.model.reg_renaming_table.{FreeList, RegRenamingTableConfig}
+import reg_renaming.model.reg_renaming_table.{FreeList, RegFile, RegFileEntry, RegRenamingTableConfig}
 
 class ModelTester extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "FreeList"
@@ -45,34 +45,62 @@ class ModelTester extends AnyFlatSpec with ChiselScalatestTester {
     assert(freeList.size() == initialSize, "FreeList size should be restored after pushing the popped element back")
   }
 
-it should "throw an exception when popping from an empty FreeList" in {
-  val emptyConfig = RegRenamingTableConfig(ptagNum = 0)
-  val emptyFreeList = new FreeList(emptyConfig)
+  it should "throw an exception when popping from an empty FreeList" in {
+    val emptyConfig = RegRenamingTableConfig(ptagNum = 0)
+    val emptyFreeList = new FreeList(emptyConfig)
 
-  intercept[IllegalArgumentException] {
-    emptyFreeList.pop()
+    intercept[IllegalArgumentException] {
+      emptyFreeList.pop()
+    }
   }
-}
 
-it should "throw an exception when pushing to a full FreeList" in {
-  val config = RegRenamingTableConfig(ptagNum = 1)
-  val freeList = new FreeList(config)
+  it should "throw an exception when pushing to a full FreeList" in {
+    val config = RegRenamingTableConfig(ptagNum = 1)
+    val freeList = new FreeList(config)
 
-  freeList.pop()
+    freeList.pop()
 
-  freeList.push(0)
+    freeList.push(0)
 
-  intercept[IllegalArgumentException] {
-    freeList.push(1)
+    intercept[IllegalArgumentException] {
+      freeList.push(1)
+    }
   }
-}
 
 
-behavior of "RegFile"
+  behavior of "RegFile"
+  it should "return a valid RegFileEntry for a valid index" in {
+    val regFile = new RegFile(RegRenamingTableConfig(ptagNum = 5))
+    val entry = new RegFileEntry(_regPtag = 1)
+    regFile.setRegFileEntry(0, entry)
+    // get and check
+    val retrievedEntry = regFile.getRegFileEntry(0)
+    assert(retrievedEntry.getRegPtag == 1, "Retrieved RegFileEntry should have a regPtag of 1")
+  }
+
+  it should "correctly set a RegFileEntry at a specific index" in {
+    val regFile = new RegFile(RegRenamingTableConfig(ptagNum = 5))
+    val newEntry = new RegFileEntry(_regPtag = 2)
+    regFile.setRegFileEntry(1, newEntry)
+
+    val setEntry = regFile.getRegFileEntry(1)
+    assert(setEntry.getRegPtag == 2, "Set RegFileEntry should have a regPtag of 2")
+  }
+
+  it should "throw an exception for an invalid index" in {
+    val config = RegRenamingTableConfig(ptagNum = 5)
+    val regFile = new RegFile(config)
+
+    assertThrows[IllegalArgumentException] {
+      regFile.getRegFileEntry(6)
+    }
+    assertThrows[IllegalArgumentException] {
+      regFile.setRegFileEntry(6, new RegFileEntry(_regPtag = 3))
+    }
+  }
 
 
-
-behavior of "RegMap"
+  behavior of "RegMap"
 
 
 }
