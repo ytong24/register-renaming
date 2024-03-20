2b0f6e2dcbb492727a7206478211ecf1283462d1
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-03-20 15:51:49 -0700

    Unfinished Chisel test for RegRenamingTable

--- src/test/scala/reg_renaming/tester/ChiselTester.scala
+++ src/test/scala/reg_renaming/tester/ChiselTester.scala
@@ -3,7 +3,8 @@ package reg_renaming.tester
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-import reg_renaming.reg_renaming_table.{FreeList, RegFile, RegFileEntryState, RegMap}
+import reg_renaming.OpConfig
+import reg_renaming.reg_renaming_table._
 
 class ChiselTester extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "FreeList"
@@ -60,6 +61,22 @@ class ChiselTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.ptagToPush.poke(dut.io.ptagPopped.peek().litValue.U) // Re-push the popped value
       dut.clock.step()
       dut.io.size.expect(5.U, "FreeList size should be restored after pushing the popped element back")
+    }
+  }
+
+  it should "pop the correct values in sequence" in {
+    test(new FreeList(ptagNum = 5)) { dut =>
+      // pop 3 values
+      for (i <- 1 to 3) {
+        dut.io.push.poke(false.B)
+        dut.io.pop.poke(true.B)
+        dut.clock.step()
+
+        val expectedValue = i.U
+        dut.io.ptagPopped.expect(expectedValue, s"Expected $expectedValue")
+      }
+
+      dut.io.size.expect((5 - 3).U, "FreeList size should decrease by 3 after popping 3 elements")
     }
   }
 
@@ -135,4 +152,126 @@ class ChiselTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.readData.expect(testValue, s"RegMap entry $testIndex should be $testValue after setPtag")
     }
   }
+
+
+  behavior of "RegRenamingTable"
+  val tableConfig = RegRenamingTableConfig(ptagNum = 16)
+  val opConfig = OpConfig(archIdNum = 8, numSrcMax = 2, numDstMax = 2)
+
+  it should "process op correctly" in {
+    test(new RegRenamingTable(tableConfig, opConfig)) { dut =>
+
+      dut.io.op.numSrc.poke(2.U)
+      dut.io.op.numDst.poke(1.U)
+
+      //      dut.io.op.in.archSrcIds.poke(VecInit(Seq(1.U, 2.U)))
+      //      dut.io.op.in.archDstIds.poke(VecInit(Seq(3.U)))
+      dut.io.op.archSrcIds(0).poke(1.U)
+      dut.io.op.archSrcIds(1).poke(2.U)
+      dut.io.op.archDstIds(0).poke(3.U)
+
+      dut.io.mode.poke(0.U)
+
+      while (!dut.io.done.peek().litToBoolean) {
+        dut.clock.step()
+      }
+
+      dut.io.op.ptagSrcIds(0).expect(1.U) // archId 1 -> ptag 1
+      dut.io.op.ptagSrcIds(1).expect(2.U) // archId 2 -> ptag 2
+      dut.io.op.ptagDstIds(0).expect(3.U) // ptag 3
+    }
+  }
+
+//  it should "process op correctly" in {
+//    test(new RegRenamingTable(tableConfig, opConfig)) { dut =>
+//      dut.io.op.in.numSrc.poke(2.U)
+//      dut.io.op.in.numDst.poke(1.U)
+//
+////      dut.io.op.in.archSrcIds.poke(VecInit(Seq(1.U, 2.U)))
+////      dut.io.op.in.archDstIds.poke(VecInit(Seq(3.U)))
+//      dut.io.op.in.archSrcIds(0).poke(1.U)
+//      dut.io.op.in.archSrcIds(1).poke(2.U)
+//      dut.io.op.in.archDstIds(0).poke(3.U)
+//
+//      dut.io.mode.poke(0.U)
+//
+//      while (!dut.io.done.peek().litToBoolean) {
+//        dut.clock.step()
+//      }
+//
+//      dut.io.op.out.ptagSrcIds(0).expect(1.U) // archId 1 -> ptag 1
+//      dut.io.op.out.ptagSrcIds(1).expect(2.U) // archId 2 -> ptag 2
+//      dut.io.op.out.ptagDstIds(0).expect(3.U) // ptag 3
+//    }
+//  }
+
+//  it should "process op correctly" in {
+//    test(new RegRenamingTable(tableConfig, opConfig)) { dut =>
+//      dut.io.op.valid.poke(true.B)
+//      dut.io.op.bits.in.numSrc.poke(2.U)
+//      dut.io.op.bits.in.numDst.poke(1.U)
+//      dut.io.op.bits.in.archSrcIds.poke(VecInit(Seq(1.U, 2.U)))
+//      dut.io.op.bits.in.archDstIds.poke(VecInit(Seq(3.U)))
+//      dut.io.mode.poke(0.U)
+//
+//      while (!dut.io.done.peek().litToBoolean) {
+//        dut.clock.step()
+//      }
+//
+//      dut.io.op.bits.out.ptagSrcIds(0).expect(1.U) // archId 1 -> ptag 1
+//      dut.io.op.bits.out.ptagSrcIds(1).expect(2.U) // archId 2 -> ptag 2
+//      dut.io.op.bits.out.ptagDstIds(0).expect(3.U) // ptag 3
+//    }
+//  }
+
+//  it should "commit op correctly" in {
+//    test(new RegRenamingTable(tableConfig, opConfig)) { dut =>
+//      // process an op
+//      dut.io.op.valid.poke(true.B)
+//      dut.io.op.bits.numSrc.poke(0.U)
+//      dut.io.op.bits.numDst.poke(1.U)
+//      dut.io.op.bits.archDstIds.poke(VecInit(1.U))
+//      dut.io.mode.poke(0.U)
+//      while (!dut.io.done.peek().litToBoolean) {
+//        dut.clock.step()
+//      }
+//      val allocatedPtag = dut.io.op.bits.ptagDstIds(0).peek()
+//
+//      // set commit signals
+//      dut.io.op.valid.poke(true.B)
+//      dut.io.op.bits.numDst.poke(1.U)
+//      dut.io.op.bits.ptagDstIds.poke(VecInit(allocatedPtag))
+//      dut.io.mode.poke(1.U)
+//
+//      // wait until finish
+//      while (!dut.io.done.peek().litToBoolean) {
+//        dut.clock.step()
+//      }
+//
+//      // check the result
+//      dut.regFile.io.index.poke(allocatedPtag)
+//      dut.regFile.io.readValue.regState.expect(RegFileEntryState.COMMIT)
+//    }
+//  }
+//
+//  it should "update available output correctly" in {
+//    test(new RegRenamingTable(tableConfig, opConfig)) { dut =>
+//      dut.io.op.valid.poke(true.B)
+//      dut.io.op.bits.numDst.poke(2.U)
+//      dut.io.mode.poke(2.U)
+//
+//      dut.io.available.expect(true.B) // should have enough available ptags at the beginning
+//
+//      dut.io.mode.poke(0.U)
+//      dut.io.op.bits.numDst.poke(tableConfig.ptagNum - 1)
+//      dut.io.op.bits.archDstIds.poke(VecInit(Seq.tabulate(tableConfig.ptagNum - 1)(_.U)))
+//      while (!dut.io.done.peek().litToBoolean) {
+//        dut.clock.step()
+//      }
+//
+//      dut.io.mode.poke(2.U)
+//      dut.io.op.bits.numDst.poke(2.U)
+//      dut.io.available.expect(false.B) // do not have enough ptags
+//    }
+//  }
 }
